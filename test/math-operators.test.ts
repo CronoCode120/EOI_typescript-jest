d18aad6a1720c0ea9f028863686f6a0ed590cb01
CronoCode120 <ctaverafernandez@gmail.com>
2023-09-07 21:59:20 +0200

    first session

---
+++ test/math-operators.test.ts
@@ -0,0 +1,34 @@
+import { sum, div, multiply, sub } from '../src/math-operators'
+
+describe('sum operation', () => {
+  it('')
+  it('sums two positive numbers', () => {
+    expect(sum(1, 2)).toStrictEqual(3);
+    expect(sum(23, 2)).toStrictEqual(25);
+  })
+  it('sums two floats', () => {
+    expect(sum(1.2, 0.7)).toStrictEqual(1.9);
+    expect(sum(2, 0.5)).toStrictEqual(2.5);
+  })
+})
+
+describe('substraction operation', () => {
+  it('substracts two positive numbers', () => {
+    expect(sub(1, 2)).toStrictEqual(3);
+    expect(sub(23, 2)).toStrictEqual(25);
+  })
+})
+
+describe('sum operation', () => {
+  it('sums two positive numbers', () => {
+    expect(sum(1, 2)).toStrictEqual(3);
+    expect(sum(23, 2)).toStrictEqual(25);
+  })
+})
+describe('sum operation', () => {
+  it('sums two positive numbers', () => {
+    expect(sum(1, 2)).toStrictEqual(3);
+    expect(sum(23, 2)).toStrictEqual(25);
+  })
+})
+  
