5031de9f2ec4673bad85d86bb784eff12e779389
CronoCode120 <ctaverafernandez@gmail.com>
2023-09-08 20:38:36 +0200

    second session

--- src/math-operators.ts
+++ src/math-operators.ts
@@ -1,16 +1,28 @@
 function sum(a: number, b: number): number {
+    if (isNaN(a) || isNaN(b)) {
+      throw new Error('A parameter is not a number (NaN)');
+    }
     return a + b;
 }
 
 function sub(a: number, b: number): number {
+    if (isNaN(a) || isNaN(b)) {
+      throw new Error('A parameter is not a number (NaN)');
+    }
     return a - b;
 }
 
 function multiply(a: number, b: number): number {
+    if (isNaN(a) || isNaN(b)) {
+      throw new Error('A parameter is not a number (NaN)');
+    }
     return a * b;
 }
 
 function div(a: number, b: number): number {
+    if (isNaN(a) || isNaN(b)) {
+      throw new Error('A parameter is not a number (NaN)');
+    }
     return a / b;
 }
 
--- test/math-operators.test.ts
+++ test/math-operators.test.ts
@@ -1,7 +1,11 @@
 import { sum, div, multiply, sub } from '../src/math-operators'
 
+const testNaNThrows = (func: (a: number, b: number) => void) => () => {
+  expect(() => func(NaN, 2)).toThrow('A parameter is not a number (NaN)');
+}
+
 describe('sum operation', () => {
-  it('')
+  it('throws when a parameter is NaN', testNaNThrows(sum))
   it('sums two positive numbers', () => {
     expect(sum(1, 2)).toStrictEqual(3);
     expect(sum(23, 2)).toStrictEqual(25);
@@ -9,26 +13,14 @@ describe('sum operation', () => {
   it('sums two floats', () => {
     expect(sum(1.2, 0.7)).toStrictEqual(1.9);
     expect(sum(2, 0.5)).toStrictEqual(2.5);
+    expect(sum(0.1, 0.2)).toStrictEqual(0.3);
   })
-})
+  it('sums negative numbers properly', () => {
+    expect(sum(4, -2)).toStrictEqual(2);
+    expect(sum(-3, -5)).toStrictEqual(-8);
+  })
+});
 
-describe('substraction operation', () => {
-  it('substracts two positive numbers', () => {
-    expect(sub(1, 2)).toStrictEqual(3);
-    expect(sub(23, 2)).toStrictEqual(25);
-  })
-})
-
-describe('sum operation', () => {
-  it('sums two positive numbers', () => {
-    expect(sum(1, 2)).toStrictEqual(3);
-    expect(sum(23, 2)).toStrictEqual(25);
-  })
-})
-describe('sum operation', () => {
-  it('sums two positive numbers', () => {
-    expect(sum(1, 2)).toStrictEqual(3);
-    expect(sum(23, 2)).toStrictEqual(25);
-  })
-})
+describe('sub', () => {
   
+})
